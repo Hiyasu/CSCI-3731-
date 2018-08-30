2b63acc7336404efa5232c78d086548328732369
Hiyasu <bmurakami007@gmail.com>
2018-08-29 17:03:26 -1000

    testing

---
+++ hw01/main.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+
+int addition(int x, int y)
+{
+		return x+y;
+}
+int main(int argc, char** argv)
+{
+	int num1=10, num2=5;
+	printf("%d\n", addition(num1, num2));
+	
+} //end of main
