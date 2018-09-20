a2ce00ab9032622520efba3251d84b6364d44d9d
Hiyasu <bmurakami007@gmail.com>
2018-09-19 15:14:03 -1000

    main

---
+++ hw04/main.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(int argc, char** argv){
+
+	FILE* fp = fopen("test.ppm" , "rb");
+	
+	int array[];
+	int width=0;
+	int height=0;
+	
+	fread(array, sizeof(int), , fp);
+	fwrite(aray, sizeof(int), , fp);
+	
+	const unsigned char* data[];
+
+	
+}
