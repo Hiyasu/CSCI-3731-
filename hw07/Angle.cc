c6e9d09008cc707104a6065e54caccbed470b858
Hiyasu <bmurakami007@gmail.com>
2018-10-18 21:20:25 -1000

    main

--- hw07/Angle.cc
+++ hw07/Angle.cc
@@ -1,36 +1,122 @@
+#include "Angle.h"
 #include <cstdio>
-
-
+#include <iostream>
 //Constructor
-Angle::Angle(int x, int y){
+Angle::Angle(double x){
 	this->x=x;
-	this->y=y;
 	
 }
 
 //Destructor
 Angle::~Angle() {
     
-    printf("destructing %lf %lf\n", x, y);
+    printf("destructing %lf %lf\n", x);
     
 }
 
 //Get methods
-int Angle::getX() const {
+double Angle::getX() const {
     
     return x;
     
 } 
 
-int Angle::getY() const {
-    
-    return y;
-    
-} 
 //Setter
-int Angle::set(int x, int y) {
+void Angle::set(double x) {
     
     this->x = x;
-    this->y = y;
-    
+}
+
+//Operator overloaders: +, -, *, /
+Angle Angle::operator+(const Angle& angle) const{
+	
+	double a=x+angle.getX();
+	while(a>=360) a-=360;
+	while(a<0) a+=360;
+	
+	return Angle(a);
+}
+
+Angle Angle::operator-(const Angle& angle) const{
+	
+	double a=x-angle.getX();
+	while(a>=360) a-=360;
+	while(a<0) a+=360;
+	
+	return Angle(a);
+}
+
+Angle Angle::operator*(const double angle) const{
+	
+	double a=x*angle;
+	while(a>=360) a-=360;
+	while(a<0) a+=360;
+	
+	return Angle(a);
+}
+
+Angle Angle::operator/(const double angle) const{
+	
+	double a=x/angle;
+	while(a>=360) a-=360;
+	while(a<0) a+=360;
+	
+	return Angle(a);
+}
+
+//Operator overloaders +=, -=, *=, /=
+const Angle& Angle::operator+=(const Angle& a){
+	this->x+=a.x;
+	
+	while(this->x>=360) this->x-=360;
+	while(this->x<0) this->x+=360;
+	
+	return *this;
+}
+
+const Angle& Angle::operator-=(const Angle& a){
+	this->x-=a.x;
+	
+	while(this->x>=360) this->x-=360;
+	while(this->x<0) this->x+=360;
+	
+	return *this;
+}
+
+const Angle& Angle::operator*=(const Angle& a){
+	this->x*=a.x;
+	
+	while(this->x>=360) this->x-=360;
+	while(this->x<0) this->x+=360;
+	
+	return *this;
+}
+
+const Angle& Angle::operator/=(const Angle& a){
+	this->x/=a.x;
+	
+	while(this->x>=360) this->x-=360;
+	while(this->x<0) this->x+=360;
+	
+	return *this;
+}
+
+
+//Operator overloader: =, ==
+const Angle& Angle::operator=(const Angle& a){
+	this->x=a.x;
+	
+	while(this->x>=360) this->x-=360;
+	while(this->x<0) this->x+=360;
+	
+	return *this;
+}
+
+bool Angle::operator==(const Angle& a)const{
+	
+	return this->x==a.x;
+}
+
+void Angle:: toString() const{
+		std::cout <<"The angle is: "<< this->x <<std::endl;
 }
